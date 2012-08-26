b09829f93892a4b1eab6bae84d2d1a9195ac9d3b
Jason Hissong <hissonjm@muohio.edu>
2012-08-26 15:48:47 -0400

    Added documentation

--- src/CatPictureAppApp.cpp
+++ src/CatPictureAppApp.cpp
@@ -1,3 +1,11 @@
+/**
+* @file CatPictureApp.app
+* Changes the color of the screen from blue to red
+*
+* @author Jason Hissong
+* @date 2012-08-26
+*/
+
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/gl.h"
 
@@ -13,6 +21,8 @@ class CatPictureAppApp : public AppBasic {
 	void draw();
 
 private:
+
+	/// red, green, and blue need to be between 0.0 and 1.0
 	float red;
 	float green;
 	float blue;
@@ -20,6 +30,7 @@ private:
 
 void CatPictureAppApp::setup()
 {
+	/// sets the screen to start at blue
 	red = 0.0f;
 	green = 0.0f;
 	blue = 1.0f;
@@ -29,16 +40,18 @@ void CatPictureAppApp::mouseDown( MouseEvent event )
 {
 }
 
-// Changes the value of the color component 
+/// Changes the value of the color component 
 void CatPictureAppApp::update()
 {
 	red = red + 0.005f;
 	blue = blue - 0.005f;
 
+	/// when red reaches 1.0, it is reset back to its original setting
 	if(red > 1.0f){
 		red = 0.0f;	
 	}
 	
+	/// when blue reaches 0.0, it is reset back to its original setting
 	if (blue < 0.0f){
 		blue = 1.0f;
 	}
@@ -46,8 +59,9 @@ void CatPictureAppApp::update()
 
 void CatPictureAppApp::draw()
 {
-	// Draws the screen with a different color based on the color value
+	/// Draws the screen with a different color based on the color value
 	gl::clear( Color(red, green, blue ) );
+
 
 }
 
